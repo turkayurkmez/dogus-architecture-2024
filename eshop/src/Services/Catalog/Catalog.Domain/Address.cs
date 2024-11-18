4a0562c6c1e056d8f2426920285dbb50f07473db
türkay ürkmez <turkay.urkmez@outlook.com>
2024-11-18 16:24:34 +0300

    eshop solution started with catalog service

---
+++ eshop/src/Services/Catalog/Catalog.Domain/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain
+{
+
+
+
+    /*
+     * NOT:
+     *  bU DOMAIN içerisinde Address diye bir nesneye ihtiyacımız olmadığı için açıklama satırı yapılmıştır.
+     */
+
+    //DDD için value object ihtiyacınızı record tipiyle karşılayabilirsiniz:
+
+
+
+    //public record Address
+    //{
+    //    public string City { get; set; }
+    //    public string Region { get; set; }
+    //    public string PostalCode { get; set; }
+    //    public string Country { get; set; }
+    //    public string Phone { get; set; }
+
+
+
+    //}
+}
---
+++ eshop/src/Services/Catalog/Catalog.Domain/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain
+{
+    public class Product : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+
+        // TODO 1: Image bilgisini Record olarak kullan. 
+
+        public void ChangePrice(decimal price)
+        {
+
+            Price = price;
+        }
+
+    }
+
+   
+}
